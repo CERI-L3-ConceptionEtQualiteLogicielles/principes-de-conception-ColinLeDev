f67eb10d9cdebdde6d64970a871fd9ed9727409e
Colin <colin.palazzetti.rubera@umontreal.ca>
2025-02-13 09:01:28 +0000

    ✨ Add SMSService and AppliSMS

---
+++ src/main/java/exo1/AppliSMS.java
@@ -0,0 +1,7 @@
+package exo1;
+
+public class AppliSMS implements SMSService {
+    public void envoiSMS(Contact contact, String message) {
+        // Logique pour envoyer un SMS
+    }
+}
---
+++ src/main/java/exo1/SMSService.java
@@ -0,0 +1,5 @@
+package exo1;
+
+public interface SMSService {
+    void envoiSMS(Contact contact, String message);
+}
