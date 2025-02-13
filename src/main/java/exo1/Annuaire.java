a5326eeb29f1f47667bbd37bb62f42b59d6ebc31
Colin <colin.palazzetti.rubera@umontreal.ca>
2025-02-13 08:23:00 +0000

    ♻️ 2a8dd0bf5c435ec6dc1152479dc8ca9839bee26d > Recreated the ContactService

---
+++ src/main/java/exo1/Annuaire.java
@@ -0,0 +1,26 @@
+package exo1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Annuaire implements ContactService {
+    private static List<Contact> lesContacts = new ArrayList<>();
+    
+    public void ajouteContact(Contact contact) {
+        lesContacts.add(contact);
+    }
+
+    public void supprimeContact(Contact contact) {
+        lesContacts.remove(contact);
+    }
+
+    public void afficheContacts() {
+        for (Contact contact : lesContacts) {
+            System.out.println("Nom: " + contact.getNom() + ", Numéro: " + contact.getNumero());
+        }
+    }
+
+    public void sauvegardeEnBD() { // Passer le dbService en arg ?
+        // Logique pour sauvegarder les contacts dans une base de données
+    }
+}
